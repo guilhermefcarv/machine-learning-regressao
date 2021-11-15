5485ed7d8fa0fd58cb487027b4c2fc0c0d670dd8
Guilherme de Carvalho <83657233+guilhermefcarv@users.noreply.github.com>
2021-11-15 10:15:53 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,28 @@
+package main;
+
+import classes.DadosRegressao;
+import classes.RegressaoLinear;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Aulas
+ */
+public class Principal {
+    
+    public static void main(String args[]) {
+        // Exemplo de dados
+        ArrayList<DadosRegressao> dadosRegressao = new ArrayList<>();
+        dadosRegressao.add(new DadosRegressao(1,1));
+        dadosRegressao.add(new DadosRegressao(2,2));
+        dadosRegressao.add(new DadosRegressao(3,3));
+        
+        RegressaoLinear r = new RegressaoLinear(dadosRegressao);
+        
+        System.out.println("Theta0 = " + r.getT0() + ", Theta1 = " + r.getT1());
+        r.calcularGradientDescent();
+        System.out.println("Theta0 = " + r.getT0() + ", Theta1 = " + r.getT1());
+        System.out.println(r.retornarPredicao(4));
+    }
+    
+}
